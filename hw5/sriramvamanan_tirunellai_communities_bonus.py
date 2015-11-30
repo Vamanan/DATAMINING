0a7b18ec2bc7c6983ca0d7ec8a9ba33a6b2311e6
csci572 <passmaster10@bitbucket.org>
2015-11-30 13:26:06 -0800

    getting there

--- hw5/sriramvamanan_tirunellai_communities_bonus.py
+++ hw5/sriramvamanan_tirunellai_communities_bonus.py
@@ -25,6 +25,16 @@ def getadjlist(g):
    adjlist[edge[0]]=[edge[1]]
  return adjlist
 
+def getedgesto(bfstree,n):
+ res=[]
+ for k in bfstree:
+  if n in bfstree[k]:
+   res.append((k,n))
+ return res
+
+def getedgesfrom(bfstree,n):
+ res=[(n,x) for x in bfstree[n]]
+ return res
 
 
 
@@ -32,10 +42,11 @@ def betweenness(g):
  #create adjacency list
  adjlist=getadjlist(g)
  print adjlist
- 
+ edgescores={}
  
  #begin bfs; create num shortest paths dict;create bfs tree
  for node in nx.nodes(g):
+  tempedgescores={}
   bfstree={}
   q=[]
   spdict={}
@@ -44,7 +55,7 @@ def betweenness(g):
   q.append(node)
   while len(q)!=0:
    head=q.pop(0)
-   for child in adjlist[q]:
+   for child in adjlist[head]:
     q.append(child)
     if head in bfstree:
      bfstree[head].append(child)
@@ -58,11 +69,42 @@ def betweenness(g):
      if splendict[child]>splendict[head]+1: #we have found a shorter path
       splendict[child]=splendict[head]+1
       spdict[child]=spdict[head]
-     elif splendict[child]=splendict[head]+1:
+     elif splendict[child]==splendict[head]+1:
       spdict[child]+=spdict[head]
   #bfs complete
   leafnodes=list(set(nx.nodes(g))-set(bfstree.keys()))
-   
+  nodescores={x:1 for x in leafnodes}
+  
+  #create level:nodes dict
+  leveldict={}
+  for k in splendict:
+   if splendict[k] not in leveldict:
+    leveldict[splendict[k]]=[k]
+   else:
+    leveldict[splendict[k]].append(k)
+  decreasing_levels=sorted(leveldict, reverse=True)
+  for l in decreasing_levels:
+   if l==0: #we are done if we have come up to the root
+    break 
+   nodes=leveldict[l]
+   for n in nodes:
+    if n not in nodescores:
+     fromedges=getedgesfrom(bfstree,n)
+     nodescores[n]=sum([tempedgescores[x] for x in fromedges])
+    edges=getedgesto(bfstree,n)
+    for e in edges:
+     edgescore=spdict[e[0]]/float(spdict[e[1]])*nodescores[e[1]]
+     tempedgescores[e]=edgescore
+     if e in edgescores:
+      edgescores[e]+=edgescore
+     else:
+      edgescores[e]=edgescore
+    
+
+ for k in edgescores:
+  edgescores[k]=edscores[k]/float(2)
+ return edgescores
+  
     
     
    
@@ -122,14 +164,14 @@ def partition():
 
 
 ipfile=sys.argv[1]
-img=sys.argv[2]
+#img=sys.argv[2]
 
 g=nx.Graph()
 
 loadGraph()
 opgraph=g.copy()
-partition()
-betweenness(opgraph)
+#partition()
+print betweenness(g)
 
 
 
